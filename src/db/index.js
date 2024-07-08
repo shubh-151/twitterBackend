deac6e20bfd6c2eaf440e35e6b6df7ce11331810
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-07-08 23:10:48 +0530

    Configuration related query

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDb connected !! DB HOST :${connectionInstance.connection.host} `);
+        console.log(`\n MongoDb chali g ho connected !! DB HOST :${connectionInstance.connection.host} `);
     } catch (error) {
         console.log("mongoDB nhi chal rha hai", error);
         process.exit(1)        
