bd4b4b4432c01abebcb32f8af749832c3af958ef
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-07-08 21:21:09 +0530

    All updates

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDb connected !! DB HOST :${connectionInstance.connection.host} `);
     } catch (error) {
-        console.log("mongoDB connection error", error);
+        console.log("mongoDB nhi chal rha hai", error);
         process.exit(1)        
     }
 }   
